04c2e87651fea29a14f9efed247435a38cb261ce
Dmitry A <adcntt@gmail.com>
2023-03-09 17:19:54 +0300

    filter panel

--- src/components/JobPosition.jsx
+++ src/components/JobPosition.jsx
@@ -3,77 +3,53 @@ import { Badge } from 'UI/Badge';
 import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
 
-const JobPosition = ({
-  id,
-  company,
-  logo,
-  new: isNew,
-  featured,
-  position,
-  role,
-  level,
-  postedAt,
-  contract,
-  location,
-  languages,
-  tools,
-}) => {
+const JobPosition = ({ id, company, logo, new: isNew, featured, position, role, level, postedAt, contract, location, languages, tools, handleAddFilter }) => {
   const badges = [].concat(role, level, ...languages, ...tools);
 
   return (
     <Card isFeatured={featured}>
       <div className='job-position'>
         <div className='job-position-info'>
-          <img
-            className='job-position-avatar'
-            src={logo}
-            alt={company}
-          />
+          <img className='job-position-avatar' src={logo} alt={company} />
           <div className='job-position-body'>
             <div className='job-postion-company'>
               <h3>{company}</h3>
               {(isNew || featured) && (
                 <Stack>
                   {isNew && (
-                    <Badge variant="rounded" colorScheme="primary">
+                    <Badge variant='rounded' colorScheme='primary'>
                       NEW!
                     </Badge>
                   )}
                   {featured && (
-                    <Badge variant="rounded" colorScheme="dark">
+                    <Badge variant='rounded' colorScheme='dark'>
                       FEATURED
                     </Badge>
                   )}
                 </Stack>
               )}
             </div>
-            <h2 className='job-position-title'>
-              {position}
-            </h2>
+            <h2 className='job-position-title'>{position}</h2>
             <Stack>
-              <div className='job-position-meta'>
-                {postedAt}
-              </div>
-              <div className='job-position-meta'>
-                {contract}
-              </div>
-              <div className='job-position-meta'>
-                {location}
-              </div>
+              <div className='job-position-meta'>{postedAt}</div>
+              <div className='job-position-meta'>{contract}</div>
+              <div className='job-position-meta'>{location}</div>
             </Stack>
           </div>
         </div>
         <Stack>
-          {badges.map(item => (
-            <Badge key={item}>{item}</Badge>
+          {badges.map((item) => (
+            <Badge key={item} onClick={() => handleAddFilter(item)}>
+              {item}
+            </Badge>
           ))}
         </Stack>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export {JobPosition};
+export { JobPosition };
 
 JobPosition.propTypes = {
   id: PropTypes.number,
@@ -89,4 +65,5 @@ JobPosition.propTypes = {
   location: PropTypes.string,
   languages: PropTypes.arrayOf(PropTypes.string),
   tools: PropTypes.arrayOf(PropTypes.string),
+  handleAddFilter: PropTypes.func,
 };
--- src/store/filters/filter-actions.js
+++ src/store/filters/filter-actions.js
@@ -2,6 +2,7 @@ export const ADD_FILTER = 'ADD_FILTER';
 export const REMOVE_FILTER = 'REMOVE_FILTER';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 
+// Action creators
 export const addFilter = (filter) => ({
   type: ADD_FILTER,
   filter,
